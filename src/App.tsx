116f8599360a52546dc73d3a728283cffe28c88d
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-08-25 10:35:58 +0200

    add speedometer screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,97 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 export default function App() {
-  const [userName, setUserName] = useState('{User Name}');
+  // const [userName, setUserName] = useState('{User Name}');
+
+  // get current date and time
+  const currentDate = new Date();
+
+  // Get various components of the current date and time
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1; 
+  const day = currentDate.getDate();
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+
+  // am or pm
+  const ampm = hours >= 12 ? 'pm' : 'am';
+
+  // Format the date and time as a string
+  const formattedDateTime = `${day} ${month} ${year} ${hours}:${minutes}${ampm}`;
+
+
 
   return (
-    <div className="container-fluid">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <input
-            type="text"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder="Hey {User Name}"
-            className="form-control mt-4 text-center"
-          />
-          <div 
-            className="position-relative bg-success h-80vh d-flex justify-content-center align-items-center border border-4 border-white border rounded-pill w-100 p-5 m-2 gap-5 "  >
-            <div className="position-relative bg-warning rounded-pill w-full h-7"></div>
-            <div className="middle-rectangle"></div>
-            <div className="position-relative bg-warning rounded-pill w-full h-7"></div>
-          </div>
+    <div className="d-flex grand-parent">
+      <div className="container main row ">
+        <div className="col-5 h-75 left-border yellow-border justify-content-center align-items-center d-flex">
+        <div className="row align-items-center">
+        <div className="col-2 h-50 bottom-border">
+                  <img src="left-indicator-light.svg" alt="left" className="img-fluid" />
+                </div>
+
+                <div className="col-10">
+                    <div className="round-div">
+                        <div className="mid-circle">
+                            <div className="child-circle d-flex justify-content-center align-items-center">
+                                <div className="p0 white-text">0</div>
+                                <div className="p10 white-text">25m</div>
+                                <div className="p25 white-text">50m</div>
+                                <div className="p35 white-text">100m</div>
+                                <div className="p50 white-text">200m</div>
+                                <div className="p65 white-text">300m</div>
+                                <div className="p75 white-text">400m</div>
+                                <div className="p85 white-text">500m</div>
+                                <div className="p100 white-text">600m</div>
+                                <div className="smallest-circle d-flex justify-content-center align-items-center ">
+                                    <div className="white-text shadow-text" style={{textAlign: 'center'}}>250 <br/> Marquis Goal</div>
+                                </div>
+                            </div>
+                        </div>
+                           
+                    </div>
+                </div>
+                
+                
+            </div>
         </div>
-      </div>
+
+        <div className="col-2 h-75 yellow-border">
+            <div className="current-date white-text pt-2">{formattedDateTime}</div>
+          center
+        </div>
+
+        <div className="col-5 h-75 right-border yellow-border  justify-content-center align-items-center d-flex">
+            <div className="row align-items-center">
+                <div className="col-10">
+                    <div className="round-div">
+                        <div className="mid-circle">
+                            <div className="child-circle d-flex justify-content-center align-items-center">
+                                <div className="p0 white-text">0%</div>
+                                <div className="p10 white-text">10%</div>
+                                <div className="p25 white-text">25%</div>
+                                <div className="p35 white-text">35%</div>
+                                <div className="p50 white-text">50%</div>
+                                <div className="p65 white-text">65%</div>
+                                <div className="p75 white-text">75%</div>
+                                <div className="p85 white-text">85%</div>
+                                <div className="p100 white-text">100%</div>
+                                <div className="smallest-circle d-flex justify-content-center align-items-center ">
+                                    <div className="white-text shadow-text" style={{textAlign: 'center'}}>55% <br/> Estimated KPI</div>
+                                </div>
+                            </div>
+                        </div>
+                           
+                    </div>
+                </div>
+                <div className="col-2 h-50 bottom-border">
+                    <img src="right-indicator-light.svg" alt="right" className="img-fluid" />
+                </div>
+            </div>
+        </div>
     </div>
+</div>
   );
 }
